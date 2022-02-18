cc1dfec39e8e3d95ad01d6fb9255141843463c23
catherinekaninni <catherinekaninni@gmail.com>
2022-02-18 21:49:45 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,24 @@
+fun main(){
+greeting("catherine")
+    modulus(43,3)
+    addition(3,6,9,15)
+    interestingfact()
+}
+
+fun greeting(name:String) {
+    println("Hello " + name)
+
+}
+fun modulus(a: Int,b: Int){
+    var modulus = a%b
+println(modulus)
+
+}
+fun addition(a:Int,b:Int,c:Int,d:Int){
+    var addition = a+b+c+d
+    println(addition)
+}
+fun interestingfact(){
+    var interestingfact = ("ilovebiking")
+    println(interestingfact)
+}
